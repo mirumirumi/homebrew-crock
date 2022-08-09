797d51d5f140b0284ab1ff07dba662d723fa7c4e
mirumi <mail@mirumi.me>
2022-08-09 21:57:12 +0900

    test: release

--- crock.rb
+++ crock.rb
@@ -1,9 +1,9 @@
 class Crock < Formula
   desc "crock is rock clock 🪨"
   homepage "https://github.com/mirumirumi/crock"
-  url "https://github.com/mirumirumi/crock/releases/download/1.0.1/crock"
-  version "1.0.1"
-  sha256 "efedbc5e202e8e3312d3a6e22811eeaf9b057171867a9ec35c9e5523772b185d"
+  url "https://github.com/mirumirumi/crock/releases/download/1.0.3/crock_darwin"
+  version "1.0.3"
+  sha256 "41626a492c5af15a361896226f765e54268368962d9323e2dc7b5af734e93524"
   license ""
 
   def install
