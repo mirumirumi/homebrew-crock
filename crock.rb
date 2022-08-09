c0838a8b57ad1de6d60c7e589cc9093d82fd1337
mirumi <mail@mirumi.me>
2022-08-09 23:05:04 +0900

    fix: sha256

--- crock.rb
+++ crock.rb
@@ -3,7 +3,7 @@ class Crock < Formula
   homepage "https://github.com/mirumirumi/crock"
   url "https://github.com/mirumirumi/crock/releases/download/1.0.3/crock_darwin"
   version "1.0.3"
-  sha256 "a8bc84e1319e3bcf01237d61fba7e1432e5fe5d56f6cf29d2a3648de8cd88497"
+  sha256 "c5c8e7bd6ae8b6207655d7de5027a6b06c9ec32c620262b15feb6e59954f80af"
   license ""
 
   def install
