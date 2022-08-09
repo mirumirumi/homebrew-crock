ebd3a00104e8439a53987a4e98996406b6365fa4
mirumi <mail@mirumi.me>
2022-08-09 23:04:27 +0900

    test: artifact name

--- crock.rb
+++ crock.rb
@@ -1,13 +1,13 @@
 class Crock < Formula
   desc "crock is rock clock 🪨"
   homepage "https://github.com/mirumirumi/crock"
-  url "https://github.com/mirumirumi/crock/releases/download/1.0.3/crock"
+  url "https://github.com/mirumirumi/crock/releases/download/1.0.3/crock_darwin"
   version "1.0.3"
   sha256 "a8bc84e1319e3bcf01237d61fba7e1432e5fe5d56f6cf29d2a3648de8cd88497"
   license ""
 
   def install
-    bin.install "crock"
+    bin.install "crock_darwin"
   end
 
   test do
